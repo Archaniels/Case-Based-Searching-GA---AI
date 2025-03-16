01737311dbe00816076b6190c7cf15d37182c260
Daniyal Arshaq S. <89289650+Archaniels@users.noreply.github.com>
2025-03-16 19:12:18 +0700

    [NEW] Penambahan Function(s)

---
+++ main.py
@@ -0,0 +1,10 @@
+## aplikasi genetic algorithm untuk memenuhi tugas besar mata kuliah kecerdasan buatan
+## dibuat oleh kelompok 11: Daniyal Arshaq Sudrajat, Riziq Rizwan.
+
+#inisialiasi
+def insialisasi_populasi():
+
+
+def main():
+    # inisialisasi populasi awal
+    populasi_awal = insialisasi_populasi()
