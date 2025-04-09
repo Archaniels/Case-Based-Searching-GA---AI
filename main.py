93f63f89343f935eb60bdf1ec310037b45c9d15e
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2025-04-09 11:44:10 +0700

    [NEW] Function Baru & Penambahan Comment Lines

--- main.py
+++ main.py
@@ -1,22 +1,58 @@
-## aplikasi genetic algorithm untuk memenuhi tugas besar mata kuliah kecerdasan buatan
-## dibuat oleh kelompok 11: Daniyal Arshaq Sudrajat, Riziq Rizwan.
+## Aplikasi genetic algorithm untuk memenuhi tugas besar mata kuliah kecerdasan buatan
+## Dibuat oleh Kelompok 11: Daniyal Arshaq Sudrajat, Riziq Rizwan.
 
-#inisialiasi
+##  =========== HAL YANG HARUS ANDA ANALISIS DAN DESAIN ===========
+    ##  Ukuran populasi, rancangan kromosom, dan cara decode-nya
+    ##  Metode pemilihan orangtua
+    ##  Metode operasi genetik (pindah silang dan mutasi)
+    ##  Probabilitas operasi genetik (𝑃𝑐 dan 𝑃𝑚)
+    ##  Metode pergantian generasi (seleksi survivor)
+    ##  Kriteria penghentian evolusi (loop)
+
+##  =========== OUTPUT PROGRAM ===========
+##  Dengan masalah yang didefinisikan di atas, output dari program Anda:
+    ##  kromosom terbaik, dan
+    ##  nilai 𝒙𝟏 dan 𝒙𝟐 hasil dekode kromosom terbaik tersebut.
+
+##  =========== PEMBAGIAN TUGAS ===========
+##  Sistem 50/50
+##  Daniyal: 
+    ## insialisasi_populasi()
+    ## dekode_kromosom()
+    ## fitness()
+    ## selection()
+##  Riziq:
+    ## def crossover():
+    ## def mutation():
+    ## def Generation_Switch():
+    ## def main():
+
+## ================================================================================================================== 
+##                                                 PROGRAM
+## ================================================================================================================== 
+
+#inisialiasi populasi
 def insialisasi_populasi():
 
+#Dekode kromosom
+def dekode_kromosom():
+
+#perhitungan fitness
 def fitness():
 
-
+#pemilihan orang tua
 def selection():
 
-
+#Crossover (pindah silang)
 def crossover():
 
-
+#mutasi
 def mutation():
 
+#penggantian generasi
+def Generation_Switch():
 
-
+#main function
 def main():
     # inisialisasi populasi awal
     populasi_awal = insialisasi_populasi()
