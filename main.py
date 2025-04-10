c7bb4e675c25aedc341bbb87e051c7c520164208
Daniyal Arshaq S. <89289650+Archaniels@users.noreply.github.com>
2025-04-10 21:55:57 +0700

    [UPDATE] Perbaikan Comment Lines

--- main.py
+++ main.py
@@ -27,6 +27,36 @@
     ## def Generation_Switch():
     ## def main():
 
+## 1. Definisi Tugas
+## Diberikan suatu fungsi untuk mencari nilai 𝑥1 dan 𝑥2 sehingga diperoleh nilai minimum dari fungsi matematis berikut:
+## 𝑓(𝑥1, 𝑥2) = − (𝑠𝑖𝑛(𝑥1)𝑐𝑜𝑠(𝑥2)tan(𝑥1 + 𝑥2) + 3/4 𝑒𝑥𝑝 (1 − √𝑥1^2)) dengan domain (batas nilai) untuk 𝑥1 dan 𝑥2 adalah −10 ≤ 𝑥1 ≤ 10 dan −10 ≤ 𝑥2 ≤ 10
+
+## Buatlah program menggunakan Algoritma Genetika (GA) untuk menyelesaikan permasalahan di atas. Program menggunakan Python, dan tidak diperbolehkan menggunakan Library yang secara langsung melakukan proses-proses pada GA, 
+## sebagaimana tercantum pada deskripsi Case Based. Lakukan analisis dan desain program GA yang Anda buat, lalu implementasikan dengan tepat.
+
+## Hal yang harus Anda analisis dan desain:
+## • Ukuran populasi, rancangan kromosom, dan cara decode-nya
+## • Metode pemilihan orangtua
+## • Metode operasi genetik (pindah silang dan mutasi)
+## • Probabilitas operasi genetik (𝑃𝑐 dan 𝑃𝑚)
+## • Metode pergantian generasi (seleksi survivor)
+## • Kriteria penghentian evolusi (loop)
+
+## Proses yang harus Anda bangun/implementasikan ke dalam baris-baris program:
+## • Inisialisasi populasi
+## • Dekode kromosom
+## • Perhitungan fitness
+## • Pemilihan orangtua
+## • Crossover (pindah silang)
+## • Mutasi
+## • Pergantian Generasi
+## Catatan: Proses-proses di atas harus dibangun tanpa menggunakan Library!
+
+## 2. Output Program
+## Dengan masalah yang didefinisikan di atas, output dari program Anda:
+## • kromosom terbaik, dan
+## • nilai 𝒙𝟏 dan 𝒙𝟐 hasil dekode kromosom terbaik tersebut
+
 ## ================================================================================================================== 
 ##                                                 PROGRAM
 ## ================================================================================================================== 
